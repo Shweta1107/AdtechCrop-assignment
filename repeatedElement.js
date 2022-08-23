efc97d36aae4e952f9a24f5d5f23447f68f67f5d
shweta1107 <shweta400@gmail.com>
2022-08-23 10:25:14 +0530

    .

--- repeatedElement.js
+++ repeatedElement.js
@@ -2,10 +2,12 @@ function repeatedElement(arr){
     for(let i=0;i<arr.length-1;i++){
         for(let j=i+1;j<arr.length;j++){
             if(arr[i]==arr[j]){
-                return arr[i];
+                // return arr[i];
+                console.log(arr[i]+" ");
             }
         }
     }
     return -1;
 }
-console.log(repeatedElement([1,2,3,4,4]));
+// console.log(repeatedElement([1,2,3,4,4]));
+repeatedElement([1,3,4,2,2]);
